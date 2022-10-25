0c101779580306eacd004c2fdba020fa47963970
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-25 19:32:07 -0300

    :rocket: finalizing the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+
+declare const $: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lading-page-evadrive';
+export class AppComponent implements AfterViewInit {
+    ngAfterViewInit(): void {
+        $('.mobile-slider').owlCarousel({
+            loop: true,
+            nav: true,
+            dots: false,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            margin: 0,
+            mouseDrag: false,
+            touchDrag: false,
+        });
+        let owlC = $('.mobile-slider');
+        owlC.owlCarousel();
+    }
+
+    public goHome(): boolean {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    }
 }
