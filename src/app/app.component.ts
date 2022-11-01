9fa49df4d2531dc83662e96ab4f16e253bb860ec
Jean Paul <jeanpaulwebb@gmail.com>
2022-11-01 18:38:09 -0300

    :sparkles: Adicionando vídeo na tela principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 declare const $: any;
 
@@ -7,22 +7,35 @@ declare const $: any;
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
+
+    public titulo: string = 'Wallace Miller';
+    public click: number = 0;
+
+    constructor() {
+        console.log('constructor chamou');
+    }
+
+    public adicioarItem(): void {
+        this.click = this.click + 1;
+    }
+
     ngAfterViewInit(): void {
-        $('.mobile-slider').owlCarousel({
-            loop: true,
-            nav: true,
-            dots: false,
-            items: 1,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-            margin: 0,
-            mouseDrag: false,
-            touchDrag: false,
-        });
-        let owlC = $('.mobile-slider');
-        owlC.owlCarousel();
+        console.log('ngAfterViewInit chamou');
+        // $('.mobile-slider').owlCarousel({
+        //     loop: true,
+        //     nav: true,
+        //     dots: false,
+        //     items: 1,
+        //     autoplay: true,
+        //     autoplayTimeout: 4000,
+        //     autoplayHoverPause: true,
+        //     margin: 0,
+        //     mouseDrag: false,
+        //     touchDrag: false,
+        // });
+        // let owlC = $('.mobile-slider');
+        // owlC.owlCarousel();
     }
 
     public goHome(): boolean {
@@ -31,4 +44,9 @@ export class AppComponent implements AfterViewInit {
         }, 500);
         return false;
     }
+
+    ngOnInit(): void {
+        console.log('ngOnInit chamou');
+    }
+
 }
